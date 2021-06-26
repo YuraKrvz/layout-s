bb816140f76ab81cc38ef28f6b95bcfd361f780f
YuraKrvz <64704180+YuraKrvz@users.noreply.github.com>
2021-06-26 23:54:36 +0300

    add website CAPPADOCIA
    add website CAPPADOCIA

---
+++ cappadocia/js/main.js
@@ -0,0 +1,8 @@
+$('.slider').slick({
+   prevArrow: '<button class="slick-btn slick-prev"><img src="images/about/slider-arrow-left.svg" alt=""></button>',
+   
+   nextArrow: '<button class="slick-btn slick-next"><img src="images/about/slider-arrow-right.svg" alt=""></button>',
+   autoplay: true,
+   // fade: true,
+});
+
